d29b01dbc21036af83f79814bf89237a14d084a7
quangzp <quangng9112@gmail.com>
2023-01-26 14:23:28 +0700

    set flask

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder='../view')
 
 @app.route("/")
 def index():
-    return "index"
+    return render_template('index.html')
 
 @app.route("/buy")
 def buy():
