f378bfd14f66656f4ccfcc872984fc3cd686e057
Minh Quang <123mntk@gmail.com>
2023-01-27 23:15:50 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, flash, jsonify
 from binance.client import Client
 import config, csv
 
@@ -16,7 +16,7 @@ def index():
     symbols = exchange_info['symbols']
     return render_template('index.html', balances = balances, symbols=symbols)
 
-@app.route("/buy", methods['POST'])
+@app.route("/buy", methods=['POST'])
 def buy():
     try:
         order = client.create_order(symbol=request.form['symbol'], 
@@ -30,8 +30,35 @@ def buy():
 
 @app.route("/sell")
 def sell():
-    return "sell"
+    try:
+        order = client.create_order(symbol=request.form['symbol'], 
+            side=SIDE_SELL,
+            type=ORDER_TYPE_MARKET,
+            quantity=request.form['quantity'])
+    except Exception as e:
+        flash(e.message, "error")
+
+    return redirect('/')
 
 @app.route("/settings")
 def settings():
     return "settings"
+
+@app.route("/history")
+def history():
+    candlesticks = client.get_historical_klines("BTCUSDT", Client.KLINE_INTERVAL_1MINUTE)
+
+    processed_candlesticks = []
+
+    for data in candlesticks:
+        candlestick = { 
+            "time": data[0] / 1000, 
+            "open": data[1],
+            "high": data[2], 
+            "low": data[3], 
+            "close": data[4]
+        }
+
+        processed_candlesticks.append(candlestick)
+
+    return jsonify(processed_candlesticks)
